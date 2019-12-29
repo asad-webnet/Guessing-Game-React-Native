8083cc9313a69e4608034627f0a400aa1a080dc1
asad-webnet <theavden12@gmail.com>
2019-12-29 20:55:29 +0500

    Added platform api to detect ios android

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,48 @@
 import React from 'react';
-import {View,Text,StyleSheet,Platform} from 'react-native';
+import {View, Text, StyleSheet, Platform} from 'react-native';
 
 import Colors from '../constants/colors'
 import TitleText from './TitleText'
 
-
 const Header = props => {
     return (
-        <View style={styles.header}>
-            <TitleText style={{...styles.headerTitle,...styles.title}}>{props.title}</TitleText>
+        <View
+            style={{
+            ...styles.headerBase,
+            ...Platform.select({ios: styles.headerIOS, android: styles.headerAndroid})
+        }}>
+            <TitleText
+                style={{
+                ...styles.headerTitle,
+                ...styles.title
+            }}>{props.title}</TitleText>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    header : {
-        width : '100%',
-        height : 90,
-        paddingTop : 36,  
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
+    headerBase: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
         alignItems: 'center',
-        justifyContent : 'center',
-        borderBottomColor : Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth : Platform.OS === 'ios' ? 3 : 0
+        justifyContent: 'center'
+    },
+    headerIOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 3
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary
     },
 
-    title : {
-        color : Platform.OS === 'ios' ? Colors.primary : 'white'
+    title: {
+        color: Platform.OS === 'ios'
+            ? Colors.primary
+            : 'white'
     }
- 
+
 });
 
 export default Header;
--- components/MainButton.js
+++ components/MainButton.js
@@ -1,27 +1,50 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    Platform
+} from 'react-native';
 
 import Colors from '../constants/colors'
 
 const MainButton = props => {
-    return <TouchableOpacity activeOpacity={0.8} onPress={props.onPress}>
-        <View style={styles.button}>
-            <Text style={styles.buttonText}>{props.children}</Text>
+
+    let ButtonComponent = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
+
+    return (
+        <View style={styles.ButtonContainer}>
+            <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>{props.children}</Text>
+                </View>
+            </ButtonComponent>
         </View>
-    </TouchableOpacity>
+
+    )
 }
 
 const styles = StyleSheet.create({
-    button : {
-        backgroundColor : Colors.primary,
-        paddingHorizontal : 30,
-        paddingVertical : 12,
-        borderRadius : 25        
+    ButtonContainer : {
+        borderRadius : 25,
+        overflow :'hidden'
     },
-    buttonText : {
-        color : 'white',
-        fontFamily : 'open-sans',
-        fontSize : 18
+    button: {
+        backgroundColor: Colors.primary,
+        paddingHorizontal: 30,
+        paddingVertical: 12,
+        borderRadius: 25
+    },
+    buttonText: {
+        color: 'white',
+        fontFamily: 'open-sans',
+        fontSize: 18
     }
 });
 
